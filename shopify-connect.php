67edba750cd094a1616671529a4141335230e538
Joey Kudish <joey@jkudish.com>
2016-08-06 21:59:27 -0400

    give proper namespacing to all functions
    also no need to check is_admin() for admin only hooks
    
    as part of code review #1

--- shopify-connect.php
+++ shopify-connect.php
@@ -28,25 +28,24 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-function remove_woocommerce_buy_buttons() {
+function shopify_wc_connect_remove_buy_buttons() {
   remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
   remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 }
-add_action( 'init', 'remove_woocommerce_buy_buttons' );
+add_action( 'init', 'shopify_wc_connect_remove_buy_buttons' );
 
-if ( is_admin() ) {  //do nothing for front end requests
-    add_action( 'load-post-new.php', 'shopify_connect_meta_box' );
-    add_action( 'load-post.php', 'shopify_connect_meta_box' );
-}
-
-function shopify_connect_meta_box() {
+function shopify_wc_connect_meta_box() {
     require_once( 'inc/metabox.php' );
     new create_shopify_meta_box();
 }
 
-function add_shopify_buttons() {
+add_action( 'load-post-new.php', 'shopify_wc_connect_meta_box' );
+add_action( 'load-post.php', 'shopify_wc_connect_meta_box' );
+
+
+function shopify_wc_connect_add_shopify_buttons() {
     echo '<p>';
     echo do_shortcode( get_post_meta( get_the_ID(), '_shopify_embed_code', true ) );
     echo '</p>';
 }
-add_action( 'woocommerce_single_product_summary', 'add_shopify_buttons', 20 );
+add_action( 'woocommerce_single_product_summary', 'shopify_wc_connect_add_shopify_buttons', 20 );
