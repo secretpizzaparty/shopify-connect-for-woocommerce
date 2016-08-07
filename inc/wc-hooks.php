f9f0ff112790c332a7e5a0fb784e838409717c03
Joey Kudish <joey@jkudish.com>
2016-08-07 19:22:54 -0400

    add shopify tab for shopify product type
    see #12

--- inc/wc-hooks.php
+++ inc/wc-hooks.php
@@ -5,3 +5,13 @@ function shopify_wc_connect_product_type_selector( $types ) {
 	$types['shopify'] = __( 'Shopify', 'shopify-wc-connect' );
 	return $types;
 }
+
+add_filter( 'woocommerce_product_data_tabs', 'shopify_wc_connect_product_tabs', 98 );
+function shopify_wc_connect_product_tabs( $tabs ) {
+	$tabs['shopify'] = array(
+		'label'  => __( 'Shopify', 'shopify-wc-connect' ),
+		'target' => 'shopify_product_options',
+		'class'  => array( 'shopify_tab', 'show_if_shopify' ),
+	);
+	return $tabs;
+}
