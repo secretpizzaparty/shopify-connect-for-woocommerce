d0da6012a5b95744fd442ad98f3c761d86fe1a2b
Joey Kudish <joey@jkudish.com>
2016-08-06 21:55:37 -0400

    move metabox class to separate file
    as part of code review #1

--- shopify-connect.php
+++ shopify-connect.php
@@ -44,43 +44,6 @@ function shopify_connect_meta_box() {
     new create_shopify_meta_box();
 }
 
-class create_shopify_meta_box {
-    public function __construct() {
-        add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
-        add_action( 'save_post', array( $this, 'save' ) );
-    }
-
-    public function add_meta_box( $post_type ) {
-        $post_types = 'product';
-        if ( $post_types = 'product') {
-            add_meta_box(
-                'shopify_embed_code',
-                'Shopify Embed Code',
-                array( $this, 'render_form'),
-                $post_type,
-                'side',
-                'low'
-            );
-        }
-    }
-
-    public function save( $post_id ) {
-        if ( array_key_exists('shopify_embed_code', $_POST ) ) {
-            update_post_meta( $post_id,
-               '_shopify_embed_code',
-                $_POST['shopify_embed_code']
-            );
-        }
-    }
-
-    public function render_form( $post ) {
-    ?>
-    <?php $value = get_post_meta( $post->ID, '_shopify_embed_code', true ); ?>
-    <textarea class="widefat" cols="50" rows="5" name="shopify_embed_code" id="shopify_embed_code"/><?php if ( isset ( $value ) ) echo $value; ?></textarea>
-    <?php
-    }
-}
-
 /* Add Shopify button to WooCommerce pages */
 function add_shopify_buttons() {
     echo '<p>';
