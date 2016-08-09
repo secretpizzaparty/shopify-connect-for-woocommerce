4bf65a1ced5f89f5c3127ddf16935283a8dd781e
Joey Kudish <joey@jkudish.com>
2016-08-09 16:55:34 -0400

    implement front-end shortcode rendering
    see #9

--- inc/front-end.php
+++ inc/front-end.php
@@ -1,14 +1,14 @@
 <?php
 
-function shopify_wc_connect_remove_buy_buttons() {
-	remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
-	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
-}
-add_action( 'init', 'shopify_wc_connect_remove_buy_buttons' );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 
-function shopify_wc_connect_add_shopify_buttons() {
-	echo '<p>';
-	echo do_shortcode( get_post_meta( get_the_ID(), '_shopify_embed_code', true ) );
-	echo '</p>';
+function shopify_wc_connect_single_product_summary() {
+	$shortcode = wp_kses_post( get_post_meta( get_the_ID(), '_shopify_embed_code', true ) );
+	if ( ! empty( $shortcode ) && has_shortcode( $shortcode, 'shopify' ) ) {
+		echo '<p>';
+		echo do_shortcode( $shortcode );
+		echo '</p>';
+	}
 }
-add_action( 'woocommerce_single_product_summary', 'shopify_wc_connect_add_shopify_buttons', 20 );
+add_action( 'woocommerce_single_product_summary', 'shopify_wc_connect_single_product_summary', 40 );
