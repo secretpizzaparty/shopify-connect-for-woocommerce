e20ddd1afe63ff1b50f66f99e12b337a0a619d67
Joey Kudish <joey@jkudish.com>
2016-08-09 14:02:41 -0400

    put shopify tab on top in write panel tabs
    see #12

--- inc/wc-hooks.php
+++ inc/wc-hooks.php
@@ -8,10 +8,13 @@ function shopify_wc_connect_product_type_selector( $types ) {
 
 add_filter( 'woocommerce_product_data_tabs', 'shopify_wc_connect_product_tabs', 98 );
 function shopify_wc_connect_product_tabs( $tabs ) {
-	$tabs['shopify'] = array(
+	unset($tabs['shipping']);
+	$shopify_tab = array( 'shopify' => array(
 		'label'  => __( 'Shopify', 'shopify-wc-connect' ),
 		'target' => 'shopify_product_options',
 		'class'  => array( 'shopify_tab', 'show_if_shopify' ),
-	);
-	return $tabs;
+	) );
+	// puts the shopify tab on top
+	return $shopify_tab + $tabs;
+}
 }
