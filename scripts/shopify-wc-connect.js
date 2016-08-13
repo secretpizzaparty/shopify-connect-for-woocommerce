91a5466f62d6e5ba54d409e5ec2342793993cb87
Joey Kudish <joey@jkudish.com>
2016-08-13 15:26:04 -0700

    remove url fragment hash after redirect
    fixes #14

--- scripts/shopify-wc-connect.js
+++ scripts/shopify-wc-connect.js
@@ -1,5 +1,6 @@
 jQuery( document ).ready( function( $ ){
 	if ( '#shopify_cart' === window.location.hash ) {
+		history.pushState( "", document.title, window.location.pathname + window.location.search );
 		setTimeout( function() {
 			$('div[data-embed_type="cart_content"]').addClass('active');
 		}, 800 );
